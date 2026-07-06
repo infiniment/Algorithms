7761fd29ec87bc5f559ccb591ac722d2b984d29e
Sumin <113510973+infiniment@users.noreply.github.com>
2026-07-06 18:43:11 +0900

    [Passed] 사각형의 총 넓이 2 (132ms, 10MB)

---
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int OFFSET = 100;
+
+        int n = sc.nextInt();
+
+        boolean[][] checked = new boolean[201][201];
+
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt() + OFFSET;
+            int y1 = sc.nextInt() + OFFSET;
+            int x2 = sc.nextInt() + OFFSET;
+            int y2 = sc.nextInt() + OFFSET;
+            
+            // 겹치는 부분
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    checked[x][y] = true;
+                }
+            }
+        }
+
+        int area = 0;
+
+        for (int x = 0; x <= 200; x++) {
+            for (int y = 0; y <= 200; y++) {
+                if (checked[x][y]) {
+                    area++;
+                }
+            }
+        }
+
+        System.out.println(area);
+    }
+}
